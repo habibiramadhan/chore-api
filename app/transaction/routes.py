e9fab568e651b813d52bfa77d2e60c4967210f8f
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2024-09-30 17:46:03 +0700

    feat: TRANSACTION-005 add update transaction feature

--- app/transaction/routes.py
+++ app/transaction/routes.py
@@ -1,15 +1,27 @@
 from fastapi import APIRouter, status, HTTPException
-from app.transaction.schemas import CreateTransactionModel
+from app.transaction.schemas import CreateTransactionModel, UpdateTransactionModel
 from app.transaction.service import TransactionService
 from app.database import db_dependency
 
 transaction_router = APIRouter()
 transaction_service = TransactionService()
 
+# POST
 @transaction_router.post("/")
 async def create_transaction(transaction:CreateTransactionModel, db : db_dependency):
     try:
         new_transaction = await transaction_service.create_transaction(transaction, db)
         return {"status": "OK", "data": new_transaction}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# PUT
+@transaction_router.put("/{transaction_id}")
+async def update_transaction(transaction_id:int, transaction_update:UpdateTransactionModel, db: db_dependency):
+    try:
+        updated_transaction = await transaction_service.update_transaction(transaction_id, transaction_update, db) 
+        if updated_transaction:
+            return {"status": "OK", "data": updated_transaction}
+        raise HTTPException(status_code=404, detail=f"Transaction-{transaction_id} doesn't exist")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- app/transaction/service.py
+++ app/transaction/service.py
@@ -1,9 +1,10 @@
 import app.models as models
-from app.transaction.schemas import CreateTransactionModel
+from app.transaction.schemas import CreateTransactionModel, UpdateTransactionModel
 from app.models import Transaction
 from app.database import db_dependency
 
 class TransactionService:
+    # create
     async def create_transaction(self, transaction: CreateTransactionModel, db : db_dependency):
         transaction_data_dict = transaction.model_dump()
         new_transaction = Transaction(**transaction_data_dict)
@@ -11,3 +12,18 @@ class TransactionService:
         db.commit()
         db.refresh(new_transaction)
         return new_transaction
+    
+    # update
+    async def update_transaction(self, transaction_id:int, transaction_update: UpdateTransactionModel, db:db_dependency):
+        transaction = db.query(Transaction).filter(Transaction.transaction_id == transaction_id).first()
+        if transaction:
+            transaction.date = transaction_update.date if transaction_update.date else transaction.date
+            transaction.name = transaction_update.name if transaction_update.name else transaction.name
+            transaction.category = transaction_update.category if transaction_update.category else transaction.category
+            transaction.description = transaction_update.description if transaction_update.description else transaction.description
+            transaction.nominal = transaction_update.nominal if transaction_update.nominal else transaction.nominal
+            
+            db.commit()
+            db.refresh(transaction)
+            return transaction
+        return
