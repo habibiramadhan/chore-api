0a55ccda34a4cf50c7d3143b701fef23e26c12e1
Muhammad Habibi Ramadhan <habibiramadhan.dev@gmail.com>
2024-09-30 17:26:47 +0700

    fix: AUTH-001 Update database.py

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
