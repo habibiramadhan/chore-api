7e70914ecb5733db1b407845a9c2c74d9c955409
Muhammad Fadhil Amri <fadhil133113@gmail.com>
2024-10-01 10:47:47 +0700

    feat: TRANSACTION-007 add user authentication to access transaction

--- app/transaction/routes.py
+++ app/transaction/routes.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from app.transaction.schemas import CreateTransactionModel, UpdateTransactionModel
 from app.transaction.service import TransactionService
 from app.database import db_dependency
+from app.auth.routes import get_current_user
 
 transaction_router = APIRouter()
 transaction_service = TransactionService()
 
 # POST
 @transaction_router.post("/")
-async def create_transaction(transaction:CreateTransactionModel, db : db_dependency):
+async def create_transaction(transaction:CreateTransactionModel, db : db_dependency, current_user: str = Depends(get_current_user)):
     try:
         new_transaction = await transaction_service.create_transaction(transaction, db)
         return {"status": "OK", "data": new_transaction}
@@ -17,7 +18,7 @@ async def create_transaction(transaction:CreateTransactionModel, db : db_depende
     
 # DELETE
 @transaction_router.delete("/{transaction_id}")
-async def delete_transaction(transaction_id:int, db:db_dependency):
+async def delete_transaction(transaction_id:int, db:db_dependency, current_user: str = Depends(get_current_user)):
     try:
         deleted_transaction = await transaction_service.delete_transaction(transaction_id, db)
         if deleted_transaction:
@@ -30,7 +31,7 @@ async def delete_transaction(transaction_id:int, db:db_dependency):
 
 # PUT
 @transaction_router.put("/{transaction_id}")
-async def update_transaction(transaction_id:int, transaction_update:UpdateTransactionModel, db: db_dependency):
+async def update_transaction(transaction_id:int, transaction_update:UpdateTransactionModel, db: db_dependency, current_user: str = Depends(get_current_user)):
     try:
         updated_transaction = await transaction_service.update_transaction(transaction_id, transaction_update, db) 
         if updated_transaction:
@@ -43,7 +44,7 @@ async def update_transaction(transaction_id:int, transaction_update:UpdateTransa
         
 # GET
 @transaction_router.get("/")
-async def get_all_transactions(db:db_dependency):
+async def get_all_transactions(db:db_dependency, current_user: str = Depends(get_current_user)):
     try:
         result = await transaction_service.get_all_transactions(db)
         return {"status": "OK", "data": result}
@@ -51,7 +52,7 @@ async def get_all_transactions(db:db_dependency):
         raise HTTPException(status_code=500, detail=str(e))
     
 @transaction_router.get("/filtered")
-async def get_transactions_filtered(*, transaction_category:str=None, transaction_name:str=None, db:db_dependency):
+async def get_transactions_filtered(*, transaction_category:str=None, transaction_name:str=None, db:db_dependency, current_user: str = Depends(get_current_user)):
     try:
         print("category:",transaction_category)
         print("name:",transaction_name)
@@ -65,7 +66,7 @@ async def get_transactions_filtered(*, transaction_category:str=None, transactio
         raise HTTPException(status_code=500, detail=str(e))    
     
 @transaction_router.get("/{transaction_id}")
-async def get_transaction_by_id(transaction_id:int, db:db_dependency):
+async def get_transaction_by_id(transaction_id:int, db:db_dependency, current_user: str = Depends(get_current_user)):
     try:
         result = await transaction_service.get_transaction_by_id(transaction_id, db)
         if result:
