f304ad80c3c219ed42cafac8ec99e4a4abe92765
Muhammad Habibi Ramadhan <habibiramadhan.dev@gmail.com>
2024-10-01 10:26:57 +0700

    fix : AUTH-007 main.py bug fix

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.database import db_dependency, engine
 from app.models import User, Base
 from app.auth.routes import router as auth_router  
 import app.models as models
+from app.transaction.routes import transaction_router
 
 app = FastAPI()
 
@@ -11,6 +12,9 @@ Base.metadata.create_all(bind=engine)
 
 # Include the authentication routes
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+
+# Include the transaction routes
+app.include_router(transaction_router, prefix="/transaction", tags=["transaction"])
 
 @app.get("/")
 def read_root():
